2790287efbaec196d09d2a18968156afd6b5b852
stm4083 <stm4083@cs.rit.edu>
2016-09-19 10:41:03 -0400

    initial commit

---
+++ CatFacts.py
@@ -0,0 +1,18 @@
+import urllib, json, time, sys, argparse, requests
+from twilio.rest import TwilioRestClient
+
+def getFact():
+    catFactUrl=""
+    response = requests.get(catFactUrl)
+    r.json()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t","--target", help="The target's phone number")
+    parser.add_argument("-m","--messages", help="The number of messages to be sent",
+                    type=int)
+    parser.add_argument("-d","--delay", help="The delay between messages (in seconds)",
+                    type=int)
+    parser.parse_args()
+
+main()
